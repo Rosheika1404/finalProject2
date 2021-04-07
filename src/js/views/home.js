e6f8a7b951fef4d68d6cab779354197943bd047c
João Henrique Xavier <jhcxavier@gmail.com>
2021-04-07 22:26:02 +0000

    adding style for login page

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,50 +3,37 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
 export const Home = () => (
-		<span>
-			<div className="sidenav">
-				<div className="login-main-text">
-					<h2>
-						Application
-						<br /> Login Page
-					</h2>
-					<p>Login or register from here to access.</p>
+	<span>
+		<div className="sidenav">
+			<div className="login-main-text">
+				<h2>
+					Application
+					<br /> Login Page
+				</h2>
+				<p>Login or register from here to access.</p>
+			</div>
+		</div>
+		<div className="main">
+			<div className="col-md-6 col-sm-12">
+				<div className="login-form">
+					<form>
+						<div className="form-group">
+							<label>Email</label>
+							<input type="text" className="form-control" placeholder="Email" />
+						</div>
+						<div className="form-group">
+							<label>Password</label>
+							<input type="password" className="form-control" placeholder="Password" />
+						</div>
+						<button type="submit" className="btn btn-black">
+							Login
+						</button>
+						<button type="submit" className="btn btn-secondary">
+							Register
+						</button>
+					</form>
 				</div>
 			</div>
-			<div className="main">
-				<div className="col-md-6 col-sm-12">
-					<div className="login-form">
-						<form>
-							<div className="form-group">
-								<label>Email</label>
-								<input
-									type="text"
-									className="form-control"
-									placeholder="Email"
-									
-								/>
-							</div>
-							<div className="form-group">
-								<label>Password</label>
-								<input
-									type="password"
-									className="form-control"
-									placeholder="Password"
-									
-								/>
-							</div>
-							<button
-								type="submit"
-								className="btn btn-black"
-								>
-								Login
-							</button>
-							<button type="submit" className="btn btn-secondary">
-								Register
-							</button>
-						</form>
-					</div>
-				</div>
-			</div>
-		</span>
+		</div>
+	</span>
 );
