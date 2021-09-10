28f59406c6f7cc78bfb4b63b2f6110bc465fec2e
Mappel21 <Mappel7676@gmail.com>
2021-09-10 16:17:51 +0000

    setting use effect

--- src/js/component/chat.js
+++ src/js/component/chat.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Chat() {
-    return(
+    const [messages, setMessages] = useState()
+    useEffect(() => {
+        db.collection('messages').orderBy('createdAt').limit(50).onSnapshot(snapshot => {
+            setMessages(snapshot.docs.map(doc => doc.data()))
+        })
+    }, [])
+    return (
         <div>
             <signOut></signOut>
-            Chat
+
         </div>
     )
 }
