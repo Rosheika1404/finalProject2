c81b199c91696220a937c9ef71443ac6667ce2fa
Mappel21 <Mappel7676@gmail.com>
2021-09-10 16:36:48 +0000

    setting message map

--- src/js/component/chat.js
+++ src/js/component/chat.js
@@ -1,7 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import { auth } from '..'
+import React, { useState, useEffect, useRef } from 'react'
+import { db, auth } from '../index'
+import SendMessage from './SendMessage'
+import signOut from'../utilities/signOut'
 
 function Chat() {
+    const scroll = useRef()
     const [messages, setMessages] = useState()
     useEffect(() => {
         db.collection('messages').orderBy('createdAt').limit(50).onSnapshot(snapshot => {
@@ -10,15 +13,19 @@ function Chat() {
     }, [])
     return (
         <div>
-            <signOut></signOut>
-            {messages.map(({ id, text, photoURL, uid }) => (
-                <div>
-                    <div key={id} className={`msg ${uid === auth.currentUser.uid ? 'sent' : 'received'}`}>
-                        <img src={photoURL} alt="" />
-                        <p>{text}</p>
+            < signOut />
+            <div className="msgs">
+                {messages.map(({ id, text, photoURL, uid }) => (
+                    <div>
+                        <div key={id} className={`msg ${uid === auth.currentUser.uid ? 'sent' : 'received'}`}>
+                            <img src={photoURL} alt="" />
+                            <p>{text}</p>
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
+            <SendMessage scroll={scroll} />
+            <div ref={scroll}></div>
         </div>
     )
 }
