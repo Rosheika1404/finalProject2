bcf299319ec6b8369ffe741c74c072460f345862
Mappel21 <Mappel7676@gmail.com>
2021-09-10 16:26:34 +0000

    setting message map

--- src/js/component/chat.js
+++ src/js/component/chat.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { auth } from '..'
 
 function Chat() {
     const [messages, setMessages] = useState()
@@ -10,7 +11,14 @@ function Chat() {
     return (
         <div>
             <signOut></signOut>
-
+            {messages.map(({ id, text, photoURL, uid }) => (
+                <div>
+                    <div key={id} className={`msg ${uid === auth.currentUser.uid ? 'sent' : 'received'}`}>
+                        <img src={photoURL} alt="" />
+                        <p>{text}</p>
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }
