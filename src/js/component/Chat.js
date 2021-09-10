65018be20c2c139f459cb270a0d4a97e7d42171f
Sheika1404 <rosheika1404@gmail.com>
2021-09-10 20:54:15 +0000

    still working on this

--- src/js/component/Chat.js
+++ src/js/component/Chat.js
@@ -1,35 +1,38 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 // import React, { useState, useEffect, useRef } from 'react'
-import { db, auth } from '../index'
-import SendMessage from './SendMessage'
-import signOut from'../utilities/signOut'
+import { db, auth } from "../index";
+import SendMessage from "./SendMessage";
+import signOut from "../utilities/signOut";
+
+import "../../styles/chat.scss";
 
 function Chat() {
-    // const scroll = useRef()
-    const [messages, setMessages] = useState([])
-    useEffect(() => {
-        db.collection('messages').orderBy('createdAt').limit(50).onSnapshot(snapshot => {
-            setMessages(snapshot.docs.map(doc => doc.data()))
-        })
-    }, [])
-    return (
-        <div>
-            < signOut />
-            <div className="msgs">
-                {messages.map(({ id, text, photoURL, uid }) => (
-                    <div>
-                        <div key={id} className={`msg ${uid === auth.currentUser.uid ? 'sent' : 'received'}`}>
-                            <img src={photoURL} alt="" />
-                            <p>{text}</p>
-                        </div>
-                    </div>
-                ))}
-            </div>
-            {/* <SendMessage scroll={scroll} /> */}
-            <SendMessage />
-            {/* <div ref={scroll}></div> */}
-        </div>
-    )
+	// const scroll = useRef()
+	const [messages, setMessages] = useState([]);
+	useEffect(() => {
+		db.collection("messages")
+			.orderBy("createdAt")
+			.limit(50)
+			.onSnapshot(snapshot => {
+				setMessages(snapshot.docs.map(doc => doc.data()));
+			});
+	}, []);
+	return (
+		<div>
+			<signOut />
+			<div className="msgs">
+				{messages.map(({ id, text, photoURL, uid }) => (
+					<div key={id} className={`msg ${uid === auth.currentUser.uid ? "sent" : "received"}`}>
+						<img src={photoURL} alt="" />
+						<p>{text}</p>
+					</div>
+				))}
+			</div>
+			{/* <SendMessage scroll={scroll} /> */}
+			<SendMessage />
+			{/* <div ref={scroll}></div> */}
+		</div>
+	);
 }
 
-export default Chat
+export default Chat;
--- src/js/component/SendMessage.js
+++ src/js/component/SendMessage.js
@@ -1,35 +1,35 @@
-import React, { useState } from "react"
-import { db, auth } from '../index'
-import { Input, Button } from '@material-ui/core'
-import firebase from "firebase"
+import React, { useState } from "react";
+import { db, auth } from "../index";
+import { Input, Button } from "@material-ui/core";
+import firebase from "firebase";
 
-// function SendMessage({ scroll }) 
+// function SendMessage({ scroll })
 function SendMessage() {
-    const [msg, setMsg] = useState('')
+	const [msg, setMsg] = useState("");
 
-    async function sendMessage(e) {
-        e.preventDefault()
-        const { uid, photoURL } = auth.currentUser
+	async function sendMessage(e) {
+		e.preventDefault();
+		const { uid, photoURL } = auth.currentUser;
 
-        await db.collection('messages').add({
-            text: msg,
-            photoURL,
-            uid,
-            createdAt: firebase.firestore.FieldValue.serverTimestamp()
-        })
-        setMsg('')
-        // scroll.current.scrollIntoView({ behavior: 'smooth' })
-    }
-    return (
-        <div>
-            <form onSubmit={sendMessage}>
-                <div className="sendMsg">
-                    <input value={msg} onChange={(e) => setMsg(e.target.value)} placeholder="Say Something..." />
-                    <Button type="submit">Send</Button>
-                </div>
-            </form>
-        </div>
-    )
+		await db.collection("messages").add({
+			text: msg,
+			photoURL,
+			uid,
+			createdAt: firebase.firestore.FieldValue.serverTimestamp()
+		});
+		setMsg("");
+		// scroll.current.scrollIntoView({ behavior: 'smooth' })
+	}
+	return (
+		<div>
+			<form onSubmit={sendMessage}>
+				<div className="sendMsg">
+					<input value={msg} onChange={e => setMsg(e.target.value)} placeholder="Say Something..." />
+					<Button type="submit">Send</Button>
+				</div>
+			</form>
+		</div>
+	);
 }
 
-export default SendMessage
+export default SendMessage;
