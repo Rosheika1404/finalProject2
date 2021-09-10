3a808f7229650d396e8f6147bea865ad13968773
Mappel21 <Mappel7676@gmail.com>
2021-09-10 17:03:21 +0000

    setting up message

---
+++ src/js/component/SendMessage.js
@@ -0,0 +1,12 @@
+function SendMessage() {
+     return(
+         <div>
+             <form>
+                 <input />
+                 <Button>Send</Button>
+             </form>
+         </div>
+     )
+}
+
+export default SendMessage
